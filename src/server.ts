c051e918dcff07d5a546030806b4378c53538d02
Cupnodes <riadlasemog@gmail.com>
2020-07-18 07:00:21 -0300

    Added eslint bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, {Request, Response} from 'express';
-import '@controllers/UsersController';
+// eslint-disable-next-line no-unused-vars
+import express, { Request, Response } from 'express'
 
-const app = express();
-const PORT = 3333;
+const app = express()
+const PORT = 3333
 
 app.get('/', (req: Request, res: Response) => {
-    res.json([{message: 'ok'}])
+  res.json([{ message: 'ok' }])
 })
 
-app.listen(PORT, (() => console.log(`Server running at port ${PORT}`)));
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
