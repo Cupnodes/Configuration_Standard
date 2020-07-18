97480af23284c22ef7dbfa2ad8881e74de69a79d
Cupnodes <riadlasemog@gmail.com>
2020-07-18 06:41:07 -0300

    Added TS paths and tsconfig-paths module

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response} from 'express';
+import '@controllers/UsersController';
 
 const app = express();
 const PORT = 3333;
 
 app.get('/', (req: Request, res: Response) => {
-    res.json([{message: 'ok'}, {channel: 'fine'}])
+    res.json([{message: 'ok'}])
 })
 
 app.listen(PORT, (() => console.log(`Server running at port ${PORT}`)));
